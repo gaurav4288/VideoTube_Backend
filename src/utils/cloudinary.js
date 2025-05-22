c2b9c936358ef30c08942ae6b484e2ddcf948fbb
Gaurav Singh <gauravsingh428826@gmail.com>
2025-05-22 11:58:22 +0530

    fileupload integration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET_KEY
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        //uplaod file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type:'auto'
+        })
+        //file has been uploaded successfully
+        console.log("File uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
